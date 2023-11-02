48999c18aa9fade8bec8ea28d9abc7ecdf92e7e1
FelipeAndresLopez <felipe.lopez@uptc.edu.co>
2023-11-01 22:39:53 -0500

    Agregar-empleados

--- Empleados/src/main/java/FelipeLopez/Empleados/controlador/IndexControlador.java
+++ Empleados/src/main/java/FelipeLopez/Empleados/controlador/IndexControlador.java
@@ -2,11 +2,13 @@ package FelipeLopez.Empleados.controlador;
 
 import FelipeLopez.Empleados.modelo.Empleado;
 import FelipeLopez.Empleados.servicio.EmpleadoServicio;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
@@ -24,6 +26,15 @@ public class IndexControlador {
         modelo.put("empleados",empleados);
         return "index";//index.jsp
     }
+    @RequestMapping(value = "/agregar", method = RequestMethod.GET)
+    public String mostrarAgregar(){
+        return "agregar";//agregar.jsp
 
+    }
+    @RequestMapping(value = "/agregar", method = RequestMethod.POST)
+    public String agregar(@ModelAttribute("empleadoForma")Empleado empleado){
+        empleadoServicio.guardarEmpleado(empleado);
+        return "redirect:/";//empleados.jsp
+    }
 
 }
